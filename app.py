445d79ceb96313fd5ab2d8f7076217727867171f
akank <akankshadec9@gmail.com>
2026-03-24 00:29:47 +0530

    fixed nltk error

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import nltk
 import streamlit as st
 import pickle
 import string
 from nltk.corpus import stopwords
-import nltk
-from nltk.stem.porter import PorterStemmer
 
+from nltk.stem.porter import PorterStemmer
+nltk.download('punkt')
+nltk.download('stopwords')
 
 ps = PorterStemmer()
 
